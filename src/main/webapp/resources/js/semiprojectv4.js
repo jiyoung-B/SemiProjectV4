cee0a48068e384fab21ee27fa1e7e78ae542e3df
jiyoung-B <catgirl0313@naver.com>
2023-05-01 16:09:40 +0900

    회원가입 - 실명확인 페이지

--- src/main/webapp/resources/js/semiprojectv4.js
+++ src/main/webapp/resources/js/semiprojectv4.js
@@ -4,9 +4,9 @@ logo?.addEventListener('click',  () => {
     location.href = '/';
 });
 
+// ---------------------------------------------
 // 이용약관 체크박스
 let agree1 = document.querySelector('#agree1');
-
 // 개인정보 이용 체크박스
 let agree2 = document.querySelector('#agree2');
 
@@ -23,3 +23,29 @@ let noagree = document.querySelector('#noagree');
 noagree?.addEventListener('click', () => {
     location.href = '/';
 });
+
+
+// --------------------------------------------- checkme
+const name2 = document.querySelector('#name2');
+const jumin1 = document.querySelector('#jumin1');
+const jumin2 = document.querySelector('#jumin2');
+const chkjumin = document.querySelector('#chkjumin');
+const chk2btn = document.querySelector('#check2btn');
+const chkfrm2 = document.querySelector('#checkfrm2');
+chk2btn?.addEventListener('click', () => {
+    if(name2.value == '') alert('이름을 입력하세요!!');
+    else if(jumin1.value == '') alert('주민번호를 입력하세요!!');
+    else if(jumin2.value == '') alert('나머지 주민번호를 입력하세요!!');
+    else if(!chkjumin.checked) alert('주민번호 처리에 동의하세요!!');
+    else {
+        chkfrm2.method='post'
+        chkfrm2.action='/join/joinme';
+        chkfrm2.submit();
+    }
+});
+
+
+
+// --------------------------------------------- joinme
+
+// --------------------------------------------- joinok
