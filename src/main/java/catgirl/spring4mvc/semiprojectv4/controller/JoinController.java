245b9e617bcfb05fd2580ce9d18c444a999c1ca3
jiyoung-B <catgirl0313@naver.com>
2023-05-01 17:13:29 +0900

    회원가입 - 회원정보 입력 페이지 1

--- src/main/java/catgirl/spring4mvc/semiprojectv4/controller/JoinController.java
+++ src/main/java/catgirl/spring4mvc/semiprojectv4/controller/JoinController.java
@@ -1,8 +1,11 @@
 package catgirl.spring4mvc.semiprojectv4.controller;
 
+import catgirl.spring4mvc.semiprojectv4.model.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/join")
@@ -19,14 +22,25 @@ public class JoinController {
         return "join/checkme.tiles";
     }
 
-    @GetMapping("/joinme")
-    public String joinme(){
-        return "join/joinme.tiles";
+    @PostMapping("/joinme")
+    public ModelAndView joinme(Member mb){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("join/joinme.tiles");
+        mv.addObject("mb", mb);
+        return mv;
 
     }
     @GetMapping("/joinok")
-    public String joinok(){
+    public String joinok2(){
         return "join/joinok.tiles";
+
+    }
+    @PostMapping("/joinok")
+    public ModelAndView joinok(Member mb){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("join/joinok.tiles");
+        mv.addObject("mb", mb);
+        return mv;
 
     }
 }
