ec28d249fab790b9c20f5876f39d9ff5a7df95f6
jiyoung-B <catgirl0313@naver.com>
2023-05-02 11:17:34 +0900

    회원가입 - 우편번호검색 RESTful 3 - dao, mybatis

---
+++ src/main/java/catgirl/spring4mvc/semiprojectv4/dao/JoinDAO.java
@@ -0,0 +1,16 @@
+package catgirl.spring4mvc.semiprojectv4.dao;
+
+import catgirl.spring4mvc.semiprojectv4.model.Member;
+import catgirl.spring4mvc.semiprojectv4.model.Zipcode;
+
+import java.util.List;
+
+public interface JoinDAO {
+    List<Zipcode> selectZipcode(String dong);
+
+    int newMember(Member m);
+    int selectOneUserid(String uid);
+
+    int selectOneMember(Member m);
+
+}
---
+++ src/main/java/catgirl/spring4mvc/semiprojectv4/dao/JoinDAOImpl.java
@@ -0,0 +1,33 @@
+package catgirl.spring4mvc.semiprojectv4.dao;
+
+import catgirl.spring4mvc.semiprojectv4.model.Member;
+import catgirl.spring4mvc.semiprojectv4.model.Zipcode;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository("jndao")
+public class JoinDAOImpl implements JoinDAO{
+    @Autowired private SqlSession sqlSession;
+
+    @Override
+    public List<Zipcode> selectZipcode(String dong) {
+        return sqlSession.selectList("join.findZipcode", dong);
+    }
+
+    @Override
+    public int newMember(Member m) {
+        return 0;
+    }
+
+    @Override
+    public int selectOneUserid(String uid) {
+        return 0;
+    }
+
+    @Override
+    public int selectOneMember(Member m) {
+        return 0;
+    }
+}
