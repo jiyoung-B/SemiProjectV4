d08ee02b5bb16968e51542738c9ff3cd57bfb0c4
jiyoung-B <catgirl0313@naver.com>
2023-05-02 17:22:25 +0900

    회원가입 - 아이디 중복체크 RESTful 5 - ajax fetch uid

--- src/main/webapp/resources/js/semiprojectv4.js
+++ src/main/webapp/resources/js/semiprojectv4.js
@@ -50,6 +50,10 @@ const sendzip = document.querySelector('#sendzip');
 const zipmodal = document.querySelector('#zipmodal');
 const zpmdbtn = document.querySelector('#zpmdbtn');
 const email3 = document.querySelector('#email3');
+const userid = document.querySelector('#userid');
+const uidmsg = document.querySelector('#uidmsg');
+const pwdmsg = document.querySelector('#pwdmsg');
+
 
 const modal = new bootstrap.Modal(zipmodal, {});
 
@@ -62,9 +66,11 @@ joinbtn?.addEventListener('click', () => {
     else if(joinfrm.addr1.value == '' || joinfrm.addr2.value == '') alert('주소를 확인하세요!!');
     else if(joinfrm.email1.value == '' || joinfrm.email2.value == '') alert('이메일을 확인하세요!!');
     else if(joinfrm.tel2.value == '' || joinfrm.tel3.value == '') alert('전화번호를 확인하세요!!');
-    else if(joinfrm.grecaptcha.value == '') alert('자동가입방지를 확인하세요!!');
+    else if(grecaptcha.getResponse() === '') alert('자동가입방지를 확인하세요!!');
     else{
-        location.href = '/join/joinok';
+        joinfrm.method = 'post';
+        joinfrm.action = '/join/joinok';
+        joinfrm.submit();
     }
 });
 zpmdbtn?.addEventListener('click', () => {
@@ -84,7 +90,7 @@ const showzipaddr = (jsons) => {
     jsons = JSON.parse(jsons);
     let addrs = '';
     jsons.forEach(function (data, idx) {
-        let bunji = (data['bunji'] !== 'null') ? data['bunji'] : '' ;
+        let bunji = (data['bunji'] !== null) ? data['bunji'] : '';
         addrs += `<option>${data['zipcode']} ${data['sido']} ${data['gugun']} ${data['dong']} ${bunji}</option>`;
     });
     while (addrlist.lastChild) {
@@ -118,7 +124,7 @@ sendzip?.addEventListener('click', () => {
     }
 });
 
-email3.addEventListener('change', () => {
+email3?.addEventListener('change', () => {
     if (email3.value === '직접입력하기'){
         joinfrm.email2.readOnly = false;
         joinfrm.email2.value = '';
@@ -128,6 +134,21 @@ email3.addEventListener('change', () => {
 
     }
 });
+dong?.addEventListener('keydown', () => {
+    if(e.keyCode === 13){ // 엔터키를 누르면
+        e.preventDefault(); // 이벤트 전파방지
+    }
+})
+userid?.addEventListener('blur', () =>{
+    if(userid.value === ''){
+        alert('중복 검색할 아이디를 입력하세요!!');
+        return;
+    }
+    const url = '/join/checkuid?uid=' + userid.value;
+    fetch(url).then(response => response.text())
+        .then(text => alert(text));
+
+})
 
 
 
